32304f71c3f7bc1df4a3b46cc17f59df3b627b13
phlsvc <phlsvc@gmail.com>
2026-03-20 22:44:44 -0300

    exercicio aula 2

---
+++ aula02-variaveis-operadores/exercicios_aula02.py
@@ -0,0 +1,10 @@
+#exercicio 1
+raio=5
+area= 3.14159 * raio**2
+print(area)
+#exercicio 2
+farenheit=int(input("Digite a temperatura em Farenheit: "))
+celsius= (farenheit - 32) * 5/9
+print(celsius)
+#exercicio 3
+
