7b19a445b1c8a3293de779d49cd6a2a35574df06
phlsvc <phlsvc@gmail.com>
2026-03-20 22:35:12 -0300

    Aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02-variaveis.py
@@ -0,0 +1,15 @@
+print("ola mundo")
+print(7 + 4)
+print("7 + 4")
+print("7"+"4")
+
+nome = "Alexandre"
+idade = 26
+peso = 70.2
+print(nome, idade, peso)
+print(f"Oii, {nome}!!!!")
+
+nome=input("Digite seu nome: ")
+idade=int(input("Digite sua idade: "))
+print(f"Olá {nome}, você tem {idade} anos")
+print(idade + 1) 
