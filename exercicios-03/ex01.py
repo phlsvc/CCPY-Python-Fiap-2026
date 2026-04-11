95c2a7fc0731fa3597347fa552c47dacd8792eb6
phlsvc <phlsvc@gmail.com>
2026-04-11 18:09:36 -0300

    edit playsound

--- exercicios-03/ex01.py
+++ exercicios-03/ex01.py
@@ -1,5 +1,3 @@
 import playsound
-def play_audio(file_path):
-    playsound.playsound(file_path)
-# Exemplo de uso
-play_audio('C:\\Users\\ventu\\OneDrive\\Área de Trabalho\\CCPY-Python-Fiap-2026\\exercicios-03\\fahhh_KcgAXfs.mp3')
+
+playsound.playsound('C:\\Users\\ventu\\OneDrive\\Área de Trabalho\\CCPY-Python-Fiap-2026\\exercicios-03\\fahhh_KcgAXfs.mp3')
